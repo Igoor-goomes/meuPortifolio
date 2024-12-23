34fa46cc8dc9cd7eaa5aeac0cecbf1491cd17367
Igor Gomes <igorgomesdebrito.dev@gmail.com>
2024-12-22 22:59:24 -0300

    dando continuidade no projeto

--- index.php
+++ index.php
@@ -6,10 +6,44 @@
     <title>Portifolio</title>
 </head>
 <body>
+    <?php
+        $nome = "Igor";
+        $saudacao = "Olá";
+        $titulo = $saudacao . "," . " Portifolio do ". $nome;
+        $subtitulo = "Seja bem vindo ao meu portifolio!";
+        $ano = 2024;
+
+        $projeto = "Meu Portifolio";
+        $status = true; // true,1 ou false,0
+        $dataProjeto = "2024-12-22";
+        $descricao = "Meu portifolio, escrito em PHP";
+    ?>
     <h1>
-        <?php 
-            echo "Portifolio do Igor";
-        ?>
+        <?php echo $titulo; ?>
     </h1>
+    <p>
+        <?php echo $subtitulo; ?>
+    </p>
+    <p>
+        <!-- Outra forma de passar código em PHP dentro do HTML! -->
+        <?=$ano; ?>
+    </p>
+    <hr>
+    <div>
+        <h2><?=$projeto;?></h2>
+        <p><?=$descricao;?></p>
+        <div>
+            <div><?=$dataProjeto;?></div>
+            <div>Projeto:
+            <?php
+                  if ($status) {
+                    echo "finalizado!";
+                  } else {
+                    echo "não finalizado";
+                  }
+                ?> 
+            </div>
+        </div>
+    </div>
 </body>
 </html>
